80c86fd420bfdfcef91cb390b4366fca838d8f26
Mikael Mårtensson <mikael.martensson@sensorsystems.se>
2020-09-08 07:34:45 +0200

    Finished rewriting mcp interface

--- firmware/module/mcp3008_interface.v
+++ firmware/module/mcp3008_interface.v
@@ -1,3 +1,4 @@
+// Interface for mcp3008 and mcp3004
 module mcp3008_interface
   (
    sample,      // sample on posedge
@@ -8,26 +9,21 @@ module mcp3008_interface
    busy,        // this interface is busy
    dout_reg,    // output data
    dout_avail,  // data is available if this is high
-   dout_accept, // data has been accapted if this is high
+   dout_accept  // data has been accapted if this is high
    );
 
-   input      dout;
-   input      sample;
-   input      dclk;
-   output reg din  = 1'b0;
-   output reg cs_n = 1'b1;
-   output reg busy = 0;
+   input             dout;
+   input             sample;
+   input             dclk;
+   output reg        din;
+   output reg        cs_n;
+   output reg        busy;
    output reg [15:0] dout_reg = 'b0;
-   output reg 	     dout_avail = 1'b1;
+   output reg 	     dout_avail;
    input 	     dout_accept;
 
-
-   // Two samples, first CH[0,1], then CH[2,3].
-   localparam number_of_samples = 3;
-   // start-bit, single-ended, CH0-2
-   reg [14:0]  mcp3008_conf = 10'b1_1_000_1_1_001_1_1_010;
-   reg [4:0]  bit_count = 0;
-   reg [1:0]  sample_count = 0;
+   localparam number_of_channels = 3;
+   reg [2:0]  channel_count = 0;
 
    reg [4:0]  state;
    localparam state_idle           = 5'b00000;
@@ -36,64 +32,184 @@ module mcp3008_interface
    localparam state_send_channel_1 = 5'b00010;
    localparam state_send_channel_2 = 5'b00110;
    localparam state_send_channel_3 = 5'b00111;
-   localparam state_wait_sample_1  = 5'b00111;
-   localparam state_null_bit       = 5'b00101;
-   localparam state_read_b9        = 5'b00100;
-   localparam state_read_b8        = 5'b01100;
-   localparam state_read_b7        = 5'b01101;
-   localparam state_read_b6        = 5'b01111;
-   localparam state_read_b5        = 5'b01110;
-   localparam state_read_b4        = 5'b01010;
-   localparam state_read_b3        = 5'b01011;
-   localparam state_read_b2        = 5'b01001;
-   localparam state_read_b1        = 5'b01000;
-   localparam state_read_b0        = 5'b11000;
-   localparam state_wait_fifo      = 5'b11001;
-   
+   localparam state_wait_sample_1  = 5'b00101;
+   localparam state_null_bit       = 5'b00100;
+   localparam state_read_b9        = 5'b01100;
+   localparam state_read_b8        = 5'b01101;
+   localparam state_read_b7        = 5'b01111;
+   localparam state_read_b6        = 5'b01110;
+   localparam state_read_b5        = 5'b01010;
+   localparam state_read_b4        = 5'b01011;
+   localparam state_read_b3        = 5'b01001;
+   localparam state_read_b2        = 5'b01000;
+   localparam state_read_b1        = 5'b11000;
+   localparam state_read_b0        = 5'b11001;
+   localparam state_wait_fifo      = 5'b11011;
+
+   // Set up on negative edge   
    always @(negedge dclk) begin
-      if (sample == 1'b1) begin
-	 busy <= 1'b1;
-      end
-      if (busy == 1'b1) begin
-	 if (bit_count == 0 )
-	   cs_n <= 1'b0;
-	 if (bit_count < 5) begin
-	    din          <= mcp3008_conf[9] ;
-	    mcp3008_conf <= {mcp3008_conf[8:0], mcp3008_conf[9]};
-	 end
-	 if (bit_count == 17)
-	   cs_n      <= 1'b1;
-	 if (bit_count == 18) begin
-	    bit_count <= 0;
-	    if (sample_count == number_of_samples - 1) begin
-	       busy      <= 1'b0;
-	       sample_count <= 0;
-	    end
-	    else
-	      sample_count <= sample_count + 1;
-	 end
-	 else
-	   bit_count <= bit_count + 1;
-      end // if (busy == 1'b1)
+      
+      state <= state_idle;
+      
+      case (state)
+
+	state_idle:
+	  if (sample)
+	    state <= state_send_start;
+	state_send_start: 
+	  state <= state_send_single;
+	state_send_single:
+	  state <= state_send_channel_1;
+	state_send_channel_1:
+	  state <= state_send_channel_2;
+	state_send_channel_2:
+	  state <= state_send_channel_3;
+	state_send_channel_3:
+	  state <= state_wait_sample_1;
+	state_wait_sample_1:
+	  state <= state_null_bit;
+	state_null_bit:
+	  state <= state_read_b9;
+	state_read_b9:
+	  state <= state_read_b8;
+	state_read_b8:
+	  state <= state_read_b7;
+	state_read_b7:
+	  state <= state_read_b6;
+	state_read_b6:
+	  state <= state_read_b5;
+	state_read_b5:
+	  state <= state_read_b4;
+	state_read_b4:
+	  state <= state_read_b3;
+	state_read_b3:
+	  state <= state_read_b2;
+	state_read_b2:
+	  state <= state_read_b1;
+	state_read_b1:
+	  state <= state_read_b0;
+	state_read_b0:
+	  state <= state_wait_fifo;
+	state_wait_fifo:
+	  if ( dout_accept == 1'b1 ) begin
+	     state <= state_idle;
+	     if ( channel_count < number_of_channels - 1 )
+	       channel_count <= channel_count + 1;
+	     else
+	       channel_count <= 0;
+	  end
+	  else
+	    state <= state_wait_fifo;
+	
+      endcase // case (state)
+
    end // always @ (negedge dclk)
 
+   always @* begin
+      din  = 1'b0;
+      cs_n = 1'b0; // default is to select chip
+      busy = 1'b1; // default is busy
+      dout_avail = 1'b0;
+
+      if ( state == state_idle ) begin
+	 busy <= 1'b0;
+	 cs_n <= 1'b1; // chip is deselected
+      end
+      if ( state == state_send_start ) begin
+	 din <= 1'b1;
+      end
+      if ( state == state_send_single ) begin
+	 din <= 1'b1; // if 1, use single-ended measurement
+      end
+      if ( state == state_send_channel_1 ) begin
+	 din <= channel_count[2];
+      end
+      if ( state == state_send_channel_2 ) begin
+	 din <= channel_count[1];
+      end
+      if ( state == state_send_channel_3 ) begin
+	 din <= channel_count[0];
+      end
+      if ( state == state_wait_fifo ) begin
+	 dout_avail <= 1'b1;
+	 cs_n       <= 1'b1; // chip is deslected
+      end
+   end
+
+   // read on positive edge
    always @(posedge dclk) begin
-      if (busy == 1'b1) begin
-	 if (bit_count < 5) begin
-	    dout_reg    <= dout_reg << 1;
-	    dout_reg[0] <= mcp3008_conf[9];
-	 end
-	 if (bit_count == 5 ) begin
-	    dout_reg    <= dout_reg << 1;
-	    dout_reg[0] <= 1'b1;
-	 end
-	 if (bit_count > 7 && bit_count < 18) begin
-	    dout_reg    <= dout_reg << 1;
-	    dout_reg[0] <= dout;
-	 end
-      end // if (busy == 1'b1)
-   end // always @ (posedge dclk)
       
+      case (state)
+
+	state_send_single: begin
+   	   dout_reg    <= dout_reg << 1;
+   	   dout_reg[0] <= din;
+	end
+	state_send_channel_1: begin
+   	   dout_reg    <= dout_reg << 1;
+   	   dout_reg[0] <= din;
+	end
+	state_send_channel_2: begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= din;
+	end
+	state_send_channel_3:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= din;
+	end
+	state_wait_sample_1:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= din;
+	end
+	state_null_bit:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= din;
+	end
+	state_read_b9:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b8:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b7:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b6:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b5:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b4:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b3:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b2:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b1:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	state_read_b0:begin
+	   dout_reg    <= dout_reg << 1;
+	   dout_reg[0] <= dout;
+	end
+	
+      endcase // case (state)
+      
+   end
+   
 
 endmodule // mcp3008_interface
 
--- firmware/testbench/mcp3008_interface_tb.v
+++ firmware/testbench/mcp3008_interface_tb.v
@@ -1,22 +1,26 @@
 module mcp3008_interface_tb();
 
-   reg      dout;
-   reg      sample;
-   reg      dclk;
-   wire     din;
-   wire     cs_n;
-   wire     busy;
-   wire [31:0] dout_reg;
+   reg         dout;
+   reg         sample;
+   reg         dclk;
+   wire        din;
+   wire        cs_n;
+   wire        busy;
+   wire [15:0] dout_reg;
+   wire        dout_avail;
+   reg 	       dout_accept;
    
    mcp3008_interface dut
      (
-      sample,  // sample on posedge
-      dclk,    // mcp3008 data clock
-      dout,    // mcp3008 data out
-      din,     // mcp3008 data in
-      cs_n,    // mcp3008 active low chip select
-      busy,    // this interface is busy
-      dout_reg // output
+      sample,      // sample on posedge
+      dclk,        // mcp3008 data clock
+      dout,        // mcp3008 data out
+      din,         // mcp3008 data in
+      cs_n,        // mcp3008 active low chip select
+      busy,        // this interface is busy
+      dout_reg,    // output data
+      dout_avail,  // data is available if this is high
+      dout_accept  // data has been accapted if this is high
       );
 
    always begin
@@ -25,14 +29,14 @@ module mcp3008_interface_tb();
 
    task mcp_wait;
       begin
-	 fork : f // implement a wait until cs_n goes high
+	 fork : f // implement a wait until dout_avail goes high
 	    begin
 	       // timeout check
 	       #2000000 $display("%t : mcp_wait timeout", $time);
 	       disable f;
 	    end
 	    begin
-	       @(negedge busy);
+	       @(posedge dout_avail);
 	       disable f;
 	    end
 	 join
@@ -41,19 +45,28 @@ module mcp3008_interface_tb();
    
    initial begin
       
-      dclk <= 0;
-      dout <= 1;
+      dclk <= 1'b0;
+      dout <= 1'b1;
+      sample <= 1'b0;
+      dout_accept <= 1'b0;
       
       $dumpfile("mcp3008_interface_tb.vcd");
       $dumpvars;
       
-      sample <= 1;
-      #10 sample <= 0;
+      sample <= 1'b1;
       mcp_wait();
+      dout_accept <= 1'b1;
+      
+      mcp_wait();
+      // dout_accept <= 1'b0;
+      
+      mcp_wait();
+      // dout_accept <= 1'b0;
+      sample <= 1'b0;
 
-      #10 sample <= 1;
-      #10 sample <= 0;
-      mcp_wait();
+      // #10 sample <= 1;
+      // #10 sample <= 0;
+      // mcp_wait();
       
       #10000 $finish;
    end
