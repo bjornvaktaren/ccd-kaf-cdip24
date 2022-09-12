2ce07b5035943f763c8ad6866783cc8c791a13ef
Mikael Mårtensson <mikma6@gmail.com>
2022-09-12 16:11:06 +0200

    Started adding formal verification for ft245 module

--- firmware/module/ft245.v
+++ firmware/module/ft245.v
@@ -25,10 +25,10 @@ module ft245
    input            ft_rxf_n;
    input            ft_txe_n;
    input            ft_clkout;
-   output reg       ft_rd_n;
-   output reg       ft_wr_n;
-   output reg       ft_siwu_n;
-   output reg       ft_oe_n;
+   output reg       ft_rd_n = 1;
+   output reg       ft_wr_n = 1;
+   output reg       ft_siwu_n = 1;
+   output reg       ft_oe_n = 1;
    output reg       tx_rinc;
    input            tx_rempty;
    input      [7:0] tx_rdata;
@@ -39,10 +39,10 @@ module ft245
  //   output wire [3:0] state_out;
  // = state;
 
-   reg [7:0] 	    tx_rdata_int;
-   reg 		    tx_rinc_int = 0;
-   reg 		    tx_rempty_int = 0;
-   reg 		    ft_wr_n_int = 0;
+   // reg [7:0] 	    tx_rdata_int;
+   // reg 		    tx_rinc_int = 0;
+   // reg 		    tx_rempty_int = 0;
+   // reg 		    ft_wr_n_int = 1;
    
    // arachne-pnr cannot infer tristate, so need to intatiate explicitly
    wire   drive_buf = ft_oe_n;
@@ -51,11 +51,13 @@ module ft245
      (
       .PACKAGE_PIN(ft_bus),
       .OUTPUT_ENABLE(drive_buf),
-      .D_OUT_0(tx_rdata_int),
+      .D_OUT_0(tx_rdata),
+      // .D_OUT_0(tx_rdata_int),
       .D_IN_0(rx_wdata)
       );
    `else
-   assign ft_bus = drive_buf ? tx_rdata_int : 8'hZZ;
+   assign ft_bus = drive_buf ? tx_rdata : 8'hZZ;
+   // assign ft_bus = drive_buf ? tx_rdata_int : 8'hZZ;
    `endif
    
    `ifndef SYNTHESIS
@@ -64,12 +66,12 @@ module ft245
        rx_wdata <= ft_bus;
    `endif
    
-   always @(negedge ft_clkout) begin
-      tx_rinc       <= tx_rinc_int;
-      ft_wr_n       <= ft_wr_n_int;
-      tx_rdata_int  <= tx_rdata;
-      tx_rempty_int <= tx_rempty;
-   end
+   // always @(posedge ft_clkout) begin
+   //    tx_rinc       <= tx_rinc_int;
+   //    ft_wr_n       <= ft_wr_n_int;
+   //    tx_rdata_int  <= tx_rdata;
+   //    tx_rempty_int <= tx_rempty;
+   // end
    
    localparam state_idle        = 4'b0000; // transition to 0001 or 1000
    localparam state_read_wait_1 = 4'b0001; //
@@ -88,10 +90,10 @@ module ft245
       case (state)
 
 	state_idle:
-	  if ( ft_txe_n == 1'b0 && tx_rempty == 1'b0 )
-	    state <= state_write_setup;
-	  else if ( ft_rxf_n == 1'b0 && rx_wfull == 1'b0 )
+	  if ( ft_rxf_n == 1'b0 && rx_wfull == 1'b0 )
 	    state <= state_read_wait_1;
+	  else if ( ft_txe_n == 1'b0 && tx_rempty == 1'b0 )
+	    state <= state_write_setup;
 
 	state_write_setup:
 	  if ( ft_txe_n == 1'b0 && tx_rempty == 1'b0 )
@@ -136,18 +138,22 @@ module ft245
       rx_winc   = 1'b0;
       busy      = 1'b0;
 
-      tx_rinc_int = 1'b0;
-      ft_wr_n_int = 1'b1;
+      // tx_rinc_int = 1'b0;
+      // ft_wr_n_int = 1'b1;
+      tx_rinc = 1'b0;
+      ft_wr_n = 1'b1;
 
       if ( state == state_write_setup ) begin
 	 busy        = 1'b1;
       end
       if ( state == state_write_1 ) begin
-	 ft_wr_n_int = 1'b0;
+	 ft_wr_n = 1'b0;
+	 // ft_wr_n_int = 1'b0;
 	 busy        = 1'b1;
       end
       if ( state == state_write_2 ) begin
-	 tx_rinc_int = 1'b1; // tell tx fifo to increase read pointer
+	 tx_rinc = 1'b1; // tell tx fifo to increase read pointer
+	 // tx_rinc_int = 1'b1; // tell tx fifo to increase read pointer
 	 busy        = 1'b1;
       end
       if ( state == state_read_wait_1 ) begin
@@ -171,5 +177,72 @@ module ft245
       
    end
    
+`ifdef FORMAL
+
+   
+   always @(*)
+     assume(ft_rxf_n != ft_txe_n);
+   
+   always @(posedge ft_clkout)
+     begin
+	// Assume that there must be 2 clock between RXF changes and TXE 
+	// changes
+	assume(ft_rxf_n != $past(ft_txe_n));
+
+	// Assume rx fifo is not full or full 1 clock after rx_winc
+	assume(rx_wfull == $past(rx_winc) || rx_wfull == 1'b0);
+	
+	// Read operation
+	if ( ft_rxf_n == 1'b0 ) begin
+	   // We cannot write
+	   assert(ft_wr_n == 1'b1);
+
+	   // We do not read if rx fifo is full
+	   if ( rx_wfull == 1'b1 ) begin
+	      assert(ft_oe_n == 1'b1);
+	      assert(ft_rd_n == 1'b1);
+	      assert(state != state_read);
+	      assert(state != state_read_wait_1);
+	      assert(state != state_read_wait_2);
+	   end
+	   
+	end
+
+	// Assume tx fifo has data or is empty 1 clock after rx_winc
+	assume(tx_rempty == $past(tx_rinc) || tx_rempty == 1'b0);
+	
+	// Write operation
+	if ( ft_txe_n == 1'b0 ) begin
+	   // We do not attempt to read
+	   assert(ft_oe_n == 1'b1);
+	   assert(ft_rd_n == 1'b1);
+
+	   // We do not write if tx fifo is empty
+	   if ( tx_rempty == 1'b1 ) begin
+	      assert(ft_wr_n == 1'b1);
+	   end
+	   
+	end
+	
+
+     end
+   
+	
+`endif   
+   // inout      [7:0] ft_bus;
+   // input            ft_rxf_n;
+   // input            ft_txe_n;
+   // input            ft_clkout;
+   // output reg       ft_rd_n;
+   // output reg       ft_wr_n;
+   // output reg       ft_siwu_n;
+   // output reg       ft_oe_n;
+   // output reg       tx_rinc;
+   // input            tx_rempty;
+   // input      [7:0] tx_rdata;
+   // output reg [7:0] rx_wdata;
+   // input 	    rx_wfull;
+   // output reg 	    rx_winc;
+   // output reg 	    busy;
 
 endmodule // ft245
